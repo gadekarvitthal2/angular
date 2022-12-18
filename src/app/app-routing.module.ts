e180891458a333c3e52b618e15f30ff3487c9fff
Vitthal Gadekar <gadekarvitthal2@gmail.com>
2022-12-18 16:35:26 +0530

    template-driven-form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { sample } from 'rxjs';
 import { ContactComponent } from './contact/contact.component';
 import { SampleComponent } from './sample/sample.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 
 const routes: Routes = [
   {path:"sample/:id/:name",component:SampleComponent},
+  {path:'template-driven',component:TemplateDrivenComponent},
 
   {path:'contact/:id/:name',component:ContactComponent}
 ];
